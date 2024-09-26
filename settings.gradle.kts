6e3d5b3ef100fe2be812db7c1dd937770a32b8a6
Zheng shaomin <daboluo719@gmail.com>
2024-09-27 00:07:32 +0800

    第三次

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,9 @@ pluginManagement {
             }
         }
         mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
         gradlePluginPortal()
     }
 }
@@ -16,6 +19,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
     }
 }
 

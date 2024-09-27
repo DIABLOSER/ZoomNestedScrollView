d24b0b23a1bf4b168b1ca907dbc2b8415fb0177c
Zheng shaomin <daboluo719@gmail.com>
2024-09-27 15:46:02 +0800

    更新数据

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 }
 group = "com.github.DIABLOSER"
-version = "1.0.1"
+version = "1.0.0"
 
 afterEvaluate {
     publishing {
@@ -51,7 +51,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.mdplus.zoomNestedScrollView"
                 artifactId = "ZoomNestedScrollView"
-                version = "1.0.1"
+                version = "1.0.0"
             }
         }
     }
